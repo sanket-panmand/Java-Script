c10d0d2f2f6dfe8c876911d58c3abd622d7e92bf
Sanket Panmand <sanketpanmand14@gmail.com>
2024-10-23 02:58:34 +0000

    arrowFunctions

---
+++ 03_basics/03_ArrowFunction.js
@@ -0,0 +1,94 @@
+// function delaclare
+
+// 1)Parameter and argument function 
+function first(num){  //parameter
+ return num
+}
+num(5)  //argument
+
+// Declaration
+function multiply(x, y) {
+    return x * y;
+  }
+
+
+  // 2) Expression
+const second = function(){
+
+}
+
+// Expression; the function is anonymous but assigned to a variable
+const multiply = function (x, y) {
+  return x * y;
+};
+// Expression; the function has its own name
+const multiply = function funcName(x, y) {
+  return x * y;
+};
+
+
+// 3) Arrow Function 
+const third = () =>{
+
+}
+// Arrow function
+const multiply = (x, y) => x * y;
+
+//4) Constructor
+const multiply = new Function("x", "y", "return x * y");
+
+
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
